1e2655e989b023b025eb84dca1ceefe4a027e436
Daniel Pombo <30377342+pombodaniel10@users.noreply.github.com>
2024-11-14 23:53:52 -0500

    Reorder structure

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,23 +1,23 @@
 import type { SiteConfig, SiteContent } from "../types";
 
 export const SITE_CONFIG: SiteConfig = {
-  title: "Daniel Pombo — Software Developer",
+  title: "Daniel Pombo - Software Developer",
   author: "Daniel Pombo Ortega",
   description:
     "Software Developer with 5 years of experience in designing, developing, and maintaining robust web applications",
   lang: "en",
   siteLogo: "/dev-small.png",
   navLinks: [
-    { text: "Experience", href: "#experience" },
     { text: "Projects", href: "#projects" },
+    { text: "Experience", href: "#experience" },
     { text: "About", href: "#about" },
   ],
   socialLinks: [
     { text: "LinkedIn", href: "https://www.linkedin.com/in/daniel-pombo-63a434194/" },
     { text: "Github", href: "https://github.com/pombodaniel10" },
   ],
-  socialImage: "/zen-og.png",
-  canonicalURL: "https://astro-zen.vercel.app",
+  socialImage: "/site-preview.png",
+  canonicalURL: "https://www.tripdpo.com",
 };
 
 export const SITE_CONTENT: SiteContent = {
@@ -28,36 +28,6 @@ export const SITE_CONTENT: SiteContent = {
       "Developer based in Cartagena, Colombia. I specialize in web application design, development and maintenance.",
     email: "pombodaniel10@gmail.com",
   },
-  experience: [
-    {
-      company: "Propelus",
-      position: "Fullstack developer",
-      startDate: "Mar 2020",
-      endDate: "Jun 2024",
-      summary: [
-        "Built new components and functionalities on the platform's website using Reacts, component-based architecture to modularize and optimize frontend codebase, facilitating easier maintenance and scalability.",
-        "Improved core platform features using Node.js, including new functionalities into APIs, developed workers to manage multiple services efficiently and integrated with databases for data management",
-        "Migrated legacy services to implement aws services to streamline server management and improve scalability, ensuring seamless transition of applications and data while optimizing performance and cost-efficiency.",
-      ],
-    },
-    {
-      company: "University of Cartagena",
-      position: "Research Developer",
-      startDate: "Feb 2019",
-      endDate: "Feb 2020",
-      summary: [
-        "Led the development of servers for data management, persistence and visualization for embedded devices using MEAN stack, with focus on the internet of things.",
-      ],
-    },
-    {
-      company: "Caiman Labs",
-      position: "CEO",
-      startDate: "Jun 2018",
-      endDate: "",
-      summary:
-        "Development of embedded systems for research groups using devices like esp8266 and arduino. Applying OOP through C++ to make software that connect sensors to the development board and share the data collected to Internet using MQTT.",
-    },
-  ],
   projects: [
     {
       name: "Landing platform starter",
@@ -114,6 +84,36 @@ export const SITE_CONTENT: SiteContent = {
       linkPreview: "/",
       linkSource: "https://github.com/pombodaniel10/tasks-manager",
       images: ["/change-status.gif"],
+    },
+  ],
+  experience: [
+    {
+      company: "Propelus",
+      position: "Fullstack developer",
+      startDate: "Mar 2020",
+      endDate: "Jun 2024",
+      summary: [
+        "Built new components and functionalities on the platform's website using Reacts, component-based architecture to modularize and optimize frontend codebase, facilitating easier maintenance and scalability.",
+        "Improved core platform features using Node.js, including new functionalities into APIs, developed workers to manage multiple services efficiently and integrated with databases for data management",
+        "Migrated legacy services to implement aws services to streamline server management and improve scalability, ensuring seamless transition of applications and data while optimizing performance and cost-efficiency.",
+      ],
+    },
+    {
+      company: "University of Cartagena",
+      position: "Research Developer",
+      startDate: "Feb 2019",
+      endDate: "Feb 2020",
+      summary: [
+        "Led the development of servers for data management, persistence and visualization for embedded devices using MEAN stack, with focus on the internet of things.",
+      ],
+    },
+    {
+      company: "Caiman Labs",
+      position: "CEO",
+      startDate: "Jun 2018",
+      endDate: "",
+      summary:
+        "Development of embedded systems for research groups using devices like esp8266 and arduino. Applying OOP through C++ to make software that connect sensors to the development board and share the data collected to Internet using MQTT.",
     },
   ],
   about: {
